f52970e6c842ed04bb1258ec5fa2cb73c326e66d
Rehtt <dsreshiram@gmail.com>
2024-03-06 16:20:55 +0800

    :sparkles: vim-easymotion

--- config.lua
+++ config.lua
@@ -5,8 +5,8 @@
 
 -- 插件
 lvim.plugins        = {
-  { 'fatih/vim-go' }, -- golang必备
-  -- { 'easymotion/vim-easymotion' }, -- 跳转
+  { 'fatih/vim-go' },              -- golang必备
+  { 'easymotion/vim-easymotion' }, -- 跳转
 
   -- 代码大纲
   {
@@ -35,6 +35,18 @@ lvim.keys.normal_mode['<C-s>'] = '<ESC>:w<CR>'          -- 保存
 lvim.keys.normal_mode['go'] = '<ESC>:AerialToggle!<CR>' -- 大纲
 lvim.keys.normal_mode['<C-q>'] = '<ESC>:q<CR>'          -- 关闭窗口
 lvim.keys.normal_mode['<M-q>'] = '<ESC>:bp | bd #<CR>'  -- 关闭buffer
+
+-- 如果不喜欢vim-easymotion默认按键可以使用下面的屏蔽和重定义
+-- lvim.builtin.which_key.mappings["j"] = {
+--   name = "jump",
+--   j = { "<Plug>(easymotion-j)", "jump to line" },
+--   w = { "<Plug>(easymotion-w)", "jump to word" }
+-- }
+-- -- 屏蔽按键
+-- vim.cmd([[
+-- map <\> <Plug>(easymotion-prefix)
+-- ]])
+
 
 lvim.transparent_window = true
 
