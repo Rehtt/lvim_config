426336c7a1aa18d114bf0e7563ae128c0045b12e
Rehtt <dsreshiram@gmail.com>
2024-05-15 09:19:16 +0800

    :sparkles: 键位

--- config.lua
+++ config.lua
@@ -80,6 +80,8 @@ lvim.keys.normal_mode['go'] = '<ESC>:AerialToggle!<CR><C-W>l' -- 打开大纲并
 lvim.keys.normal_mode['<C-q>'] = '<ESC>:q<CR>'                -- 关闭窗口
 lvim.keys.normal_mode['<M-q>'] = '<ESC>:bp | bd #<CR>'        -- 关闭buffer
 lvim.keys.normal_mode['gm'] = '<ESC>:MinimapToggle<CR>'       -- 打开/关闭代码地图
+
+-- hadronized/hop.nvim 快捷键
 lvim.builtin.which_key.mappings['j'] = {
   name = 'jump',
   l = { ':HopLineMW<CR>', 'jump to line' },
@@ -93,12 +95,16 @@ lvim.builtin.which_key.mappings['S'] = {              -- 滚动
   n = { ':set noscrollbind<CR>', 'set noscrollbind' } -- 取消当前页面同步滚动
 }
 lvim.keys.term_mode['<ESC><ESC>'] = [[<C-\><C-n>]]    -- 终端中进入normal_mode
+
+-- wfxr/vim-interestingwords 快捷键
 lvim.builtin.which_key.mappings['k'] = {
   name = "Highlight",
   k = { ":call InterestingWords('n')<cr>", "InterestingWords('n')" }, -- 高亮单词
   v = { ":call InterestingWords('v')<cr>", "InterestingWords('v')" }, -- 高亮选中段落
   K = { ":call UncolorAllWords()<cr>", "UncolorAllWords()" }          -- 取消高亮
 }
+lvim.keys.normal_mode['n'] = '<ESC>:call WordNavigation(1)<cr>'
+lvim.keys.normal_mode['N'] = '<ESC>:call WordNavigation(0)<cr>'
 
 
 lvim.transparent_window = true
