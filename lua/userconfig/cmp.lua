bd980238e9c141b88062519ec23b6453c5838a41
Rehtt <dsreshiram@gmail.com>
2024-12-20 14:58:38 +0800

    cmp 光标默认第一个

--- lua/userconfig/cmp.lua
+++ lua/userconfig/cmp.lua
@@ -14,5 +14,5 @@ lvim.builtin.cmp.formatting.format               = function(entry, vim_item)
 end
 
 -- 修改代码提示器光标逻辑
--- lvim.builtin.cmp.preselect                       = "none"
+lvim.builtin.cmp.preselect                       = "none"
 lvim.builtin.cmp.completion.completeopt          = "menu,menuone,noinsert"
