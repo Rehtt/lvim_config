55084a20bd10c4e7d597053a20c45c44753589f5
Rehtt <dsreshiram@gmail.com>
2025-02-20 17:40:56 +0800

    停用折叠插件

--- lua/userconfig/plugin.lua
+++ lua/userconfig/plugin.lua
@@ -76,52 +76,52 @@ lvim.plugins = {
     'dstein64/vim-startuptime'
   },
 
-  -- 折叠代码
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = {
-      'kevinhwang91/promise-async',
-    },
-    event = "BufReadPost",
-    config = function()
-      require("ufo").setup({
-        provider_selector = function(bufnr, filetype, buftype)
-          return { "treesitter", "indent" }
-        end,
-        fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-          local newVirtText = {}
-          local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-          local sufWidth = vim.fn.strdisplaywidth(suffix)
-          local targetWidth = width - sufWidth
-          local curWidth = 0
-          for _, chunk in ipairs(virtText) do
-            local chunkText = chunk[1]
-            local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            if targetWidth > curWidth + chunkWidth then
-              table.insert(newVirtText, chunk)
-            else
-              chunkText = truncate(chunkText, targetWidth - curWidth)
-              local hlGroup = chunk[2]
-              table.insert(newVirtText, { chunkText, hlGroup })
-              chunkWidth = vim.fn.strdisplaywidth(chunkText)
-              -- str width returned from truncate() may less than 2nd argument, need padding
-              if curWidth + chunkWidth < targetWidth then
-                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-              end
-              break
-            end
-            curWidth = curWidth + chunkWidth
-          end
-          table.insert(newVirtText, { suffix, 'MoreMsg' })
-          return newVirtText
-        end
-      })
-      vim.opt.foldenable = true     -- 保留折叠功能
-      vim.opt.foldlevel = 99        -- 设置初始折叠级别为 99
-      vim.opt.foldlevelstart = 99   -- 启动时展开所有折叠
-      vim.opt.foldcolumn = "auto:9" -- 显示一列折叠标志
-      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep:│,foldclose:]]
-    end
-  },
+  -- -- 折叠代码 按需打开，觉得先熟悉自带的折叠命令比较好
+  -- {
+  --   'kevinhwang91/nvim-ufo',
+  --   dependencies = {
+  --     'kevinhwang91/promise-async',
+  --   },
+  --   event = "BufReadPost",
+  --   config = function()
+  --     require("ufo").setup({
+  --       provider_selector = function(bufnr, filetype, buftype)
+  --         return { "treesitter", "indent" }
+  --       end,
+  --       fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+  --         local newVirtText = {}
+  --         local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+  --         local sufWidth = vim.fn.strdisplaywidth(suffix)
+  --         local targetWidth = width - sufWidth
+  --         local curWidth = 0
+  --         for _, chunk in ipairs(virtText) do
+  --           local chunkText = chunk[1]
+  --           local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+  --           if targetWidth > curWidth + chunkWidth then
+  --             table.insert(newVirtText, chunk)
+  --           else
+  --             chunkText = truncate(chunkText, targetWidth - curWidth)
+  --             local hlGroup = chunk[2]
+  --             table.insert(newVirtText, { chunkText, hlGroup })
+  --             chunkWidth = vim.fn.strdisplaywidth(chunkText)
+  --             -- str width returned from truncate() may less than 2nd argument, need padding
+  --             if curWidth + chunkWidth < targetWidth then
+  --               suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+  --             end
+  --             break
+  --           end
+  --           curWidth = curWidth + chunkWidth
+  --         end
+  --         table.insert(newVirtText, { suffix, 'MoreMsg' })
+  --         return newVirtText
+  --       end
+  --     })
+  --     vim.opt.foldenable = true     -- 保留折叠功能
+  --     vim.opt.foldlevel = 99        -- 设置初始折叠级别为 99
+  --     vim.opt.foldlevelstart = 99   -- 启动时展开所有折叠
+  --     vim.opt.foldcolumn = "auto:9" -- 显示一列折叠标志
+  --     vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep:│,foldclose:]]
+  --   end
+  -- },
 
 }
